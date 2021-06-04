b52e1fc80d6ca195b72dd2f8a9aff5cf2159a346
Gregory <greg.verissimo@gmail.com>
2021-06-04 16:44:08 +0200

    First commit

---
+++ src/test/java/com/example/patatapp/PatatappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.patatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatatappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
